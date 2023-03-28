1612328dfe7969cd4e23bbbd0d11cc8b8f6971ad
Mahmud2640 <mdmahmud484@gmail.com>
2023-03-28 13:27:00 +0600

    live chat setup

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -13,55 +13,54 @@ export const Header = () => {
       <nav>
         <ul>
           <li>
-            <a href="#" class="logo">
-              <img src="/Image/Logo.png" alt="Logo" />
+            <a href="/" class="logo">
               <span class="nav-item">AircityBD</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="/home">
               <i class="fas fa-home"></i>
               <span class="nav-item">Home</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="/profile">
               <i class="fas fa-user"></i>
               <span class="nav-item">Profile</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="/wallet">
               <i class="fas fa-wallet"></i>
               <span class="nav-item">Wallet</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="/analytices">
               <i class="fas fa-chart-bar"></i>
               <span class="nav-item">Analytics</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="/task">
               <i class="fas fa-tasks"></i>
               <span class="nav-item">Task</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="/setting">
               <i class="fas fa-cog"></i>
               <span class="nav-item">Setting</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="/help">
               <i class="fas fa-question-circle"></i>
               <span class="nav-item">Help</span>
             </a>
           </li>
           <li>
-            <a href="#" class="logout">
+            <a href="/logout" class="logout">
               <i class="fas fa-sign-in-alt"></i>
               <span class="nav-item">Log Out</span>
             </a>
